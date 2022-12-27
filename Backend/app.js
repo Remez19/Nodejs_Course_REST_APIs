5ebc1506221dfd1f952da12528ebc2c01ad1ba1d
Remez19 <rdoneplus@gmail.com>
2022-12-27 16:16:46 +0200

    Static Images & Error Handling

--- Backend/app.js
+++ Backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 require("dotenv").config();
@@ -14,6 +15,13 @@ const app = express();
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+// To serve the images folder statically
+// we need to use the path core module
+// to construct a absolute path to images folder
+// __dirname = > the path to the app.js file
+// Second argument is the path to the folder we want to serve
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -25,6 +33,16 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+// Erorr middleware to catch requests that ended up
+// in a error.
+app.use((error, req, res, next) => {
+  console.log(error);
+  // if error.statusCode not defined than 500 will be default
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message });
+});
 
 mongoose
   .connect(MONGODB_URI)
