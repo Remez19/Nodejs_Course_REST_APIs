bd354fd7558579e76532d064eb4acbd1f19043d0
Remez19 <rdoneplus@gmail.com>
2022-12-28 11:28:32 +0200

    Signing Users Up

--- Backend/app.js
+++ Backend/app.js
@@ -74,7 +74,7 @@ app.use((error, req, res, next) => {
   // if error.statusCode not defined than 500 will be default
   const status = error.statusCode || 500;
   const message = error.message;
-  const data = errors.data;
+  const data = error.data;
   res.status(status).json({ message, data });
 });
 
--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,5 +1,8 @@
 const User = require("../models/user");
 const { validationResult } = require("express-validator/check");
+const bcrypt = require("bcryptjs");
+
+const SALT_VALUE = 12;
 
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
@@ -10,4 +13,25 @@ exports.signup = (req, res, next) => {
     throw error;
   }
   const { email, name, password } = req.body;
+  bcrypt
+    .hash(password, SALT_VALUE)
+    .then((hashedPasword) => {
+      const user = new User({
+        email,
+        password: hashedPasword,
+        name,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res
+        .status(201)
+        .json({ message: "User created successfully.", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
